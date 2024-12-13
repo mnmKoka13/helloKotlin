fbc6743c9d3cb7eaaccc50b79fb61cee9856ca7b
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-13 22:28:06 +0900

    first commit

---
+++ src/TypeIntro.kt
@@ -0,0 +1,17 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Array<String>) {
+    val playerName = "Estragon"
+    var experiencePoints = 5
+    experiencePoints += 5
+    println(experiencePoints)
+    println(playerName)
+
+    var hasSteed = false
+    var pubName = "Unicorn's Horn"
+    var publicanName = "Guston"
+    var moneyInPossession = 50
+    var drinks = listOf("mead", "wine", "LaCroix")
+
+    var magicMirror = playerName.reversed()
+}
