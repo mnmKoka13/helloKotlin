8a81e343231ee0f81cb62f62637e039cb44dfb7d
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-18 23:39:44 +0900

    24/12/18 関数の型推論

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,9 +1,17 @@
 fun main(arg: Array<String>) {
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+//    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+//        val currentYear = 2024
+//        println("Adding $numBuildings houses")
+//        "Welcome to SimVillage, $playerName!(copyright $currentYear)"
+//    }
+
+    // 型推論を使う
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2024
         println("Adding $numBuildings houses")
-        "Welcome to SimVillage, $playerName!(copyright $currentYear)"
+        "Welcome to SimVillage, $playerName(copyright $currentYear)"
     }
+
 //    val greetingFunction: (String) -> String = {
 //        val currentYear = 2024
 //        "Welcome to SimVillage, $it!(copyright $currentYear)"
