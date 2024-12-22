585fda6bfa0eb8048205ac9814908a1a11cb9324
Manami Kokado <kokado313.mnm@gmail.com>
2024-12-22 19:15:56 +0900

    24/12/22 戻り値の型としての関数型

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,43 +1,21 @@
 fun main(arg: Array<String>) {
-//    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
-//        val currentYear = 2024
-//        println("Adding $numBuildings houses")
-//        "Welcome to SimVillage, $playerName!(copyright $currentYear)"
-//    }
+    runSimulation()
+}
 
-    // 型推論を使う
-//    val greetingFunction = { playerName: String, numBuildings: Int ->
-//        val currentYear = 2024
-//        println("Adding $numBuildings houses")
-//        "Welcome to SimVillage, $playerName(copyright $currentYear)"
-//    }
+fun runSimulation() {
+    val greetingFunction = configureGreetingFunction()
+    println(greetingFunction("Guyal"))
+}
 
-//    val greetingFunction: (String) -> String = {
-//        val currentYear = 2024
-//        "Welcome to SimVillage, $it!(copyright $currentYear)"
-//    }
-//    runSimulation("Guyal", greetingFunction)
-
-    // 略記構文
-    // 第二引数：関数リファレンス
-    // 第三引数：ラムダ
-    runSimulation("Guyal", ::printConstructionCost) { playerName, numBuildings ->
+// 関数型定義 = 無名関数（ラムダ）を返す
+// 一種の関数ファクトリー
+fun configureGreetingFunction(): (String) -> String {
+    val structureType = "hospitals"
+    var numBuildings = 5
+    return { playerName: String ->
         val currentYear = 2024
-        println("Adding $numBuildings houses")
-        "Welcome to SimVillage, $playerName!(copyright $currentYear)"
+        numBuildings += 1
+        println("Adding $numBuildings $structureType")
+        "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-}
-
-// 関数を受け取る関数
-inline fun runSimulation(playerName: String,
-                         costPrinter: (Int) -> Unit,
-                         greetingFunction: (String, Int) -> String) {
-    val numBuildings = (1..3).shuffled().last()
-    costPrinter(numBuildings)
-    println(greetingFunction(playerName, numBuildings))
-}
-
-fun printConstructionCost(numBuildings: Int) {
-    val cost = 500
-    println("construction cost: ${cost * numBuildings}")
 }
