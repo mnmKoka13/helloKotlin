5f772ea69df69e244288d9fe0fe78e2f4ea68a7d
Manami Kokado <kokado313.mnm@gmail.com>
2025-03-11 21:24:14 +0900

    25/03/11 エクステンション-スーパークラスに対するエクステンション

--- src/Extentions.kt
+++ src/Extentions.kt
@@ -3,6 +3,10 @@
 // 拡張関数：fun レシーバ型.関数名(引数) { 処理 }
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
+// Anyの拡張関数は全ての型について直接呼び出すことができる。
+fun Any.easyPrint() = println(this)
+
 fun main(args: Array<String>) {
-    println("Madrigal has left the building".addEnthusiasm())
+    "Madrigal has left the building".addEnthusiasm().easyPrint()
+    42.easyPrint()
 }
