fddf1e708e41c924f52a1f79f2de02ff6f5bcd90
Manami Kokado <kokado313.mnm@gmail.com>
2025-03-23 16:01:45 +0900

    25/03/23 kotlinでPaiza

--- src/PaizaChallenge.kt
+++ src/PaizaChallenge.kt
@@ -1,30 +1,41 @@
+import kotlin.math.abs
+
+//10 3
+//24
+//35
+//3
+
 fun main(args: Array<String>) {
-    val n = readLine()!!.toInt()
-    val pitchTypes = mutableListOf<String>()
-    for (i in 1 .. n) {
-        pitchTypes.add(readLine()!!)
-    }
+    val firstLine = readln().split(" ")
+    val n = firstLine[0].toInt()
+    val m = firstLine[1].toInt()
 
-    var strikeCount = 0
-    var ballCount = 0
-
-    for (pitchType in pitchTypes) {
-        if ("strike".equals(pitchType)) {
-            if (strikeCount < 2) {
-                strikeCount++
-                println("strike!")
-            } else {
-                println("out!")
-                return
-            }
-        } else if ("ball".equals(pitchType)) {
-            if (ballCount < 3) {
-                ballCount++
-                println("ball!")
-            } else {
-                println("fourball!")
-                return
-            }
+    for (i in 1..m) {
+        val weight = readln().toInt()
+        if (weight < n) {
+            println(n)
+            continue
         }
+        val quotient: Int = weight / n
+
+        if (abs((quotient * n) - weight) < abs(((quotient + 1) * n) - weight)) {
+            println(quotient * n)
+        } else {
+            println((quotient + 1) * n)
+        }
+
     }
 }
+
+
+// 71,142,213,284,355,426,497,568,639,710,781,852,923,994,1065,1136,1207,1278,1349,1420
+
+//71 8
+//804 -> 781
+//969 -> 994
+//803 -> 781
+//814 -> 852
+//7 -> 71
+//140 -> 142
+//107 -> 142
+//106 -> 142
