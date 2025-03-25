81d63b4c2e2bf29de6a72e0455d32897c9af91aa
Manami Kokado <kokado313.mnm@gmail.com>
2025-03-25 22:07:48 +0900

    25/03/25 kotlinでぱいざ

--- src/PaizaChallenge.kt
+++ src/PaizaChallenge.kt
@@ -1,7 +1,25 @@
 fun main(args: Array<String>) {
-    val input = readln().toInt()
-    val binary = Integer.toBinaryString(input)
-
-    val result = binary.count() { it == '1'}
+    val (n, m) = readln().split(" ").map { it.toInt() } // n: 座席数　m: グループ数
+    var seats = MutableList(n) { 0 }
+    for (i in 1..m) {
+        val copiedSeats = seats.toList().toMutableList()
+        val (a, b) = readln().split(" ").map { it.toInt() } // a: 人数 b: 座席番号
+        var sitFlg = true
+        var nextSeat = b - 1
+        for (j in 0 until a) {
+            if (copiedSeats[nextSeat] == 1) {
+                sitFlg = false
+                break
+            }
+            copiedSeats[nextSeat] = 1
+            nextSeat = if (nextSeat + 1 >= n) 0 else nextSeat + 1
+        }
+        if (sitFlg) {
+            seats = copiedSeats
+        }
+    }
+    val result = seats.sum()
     println(result)
 }
+
+
