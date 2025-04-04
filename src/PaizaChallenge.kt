17be2a5efe7a2a0bc19baf78d63e517168957b18
Manami Kokado <kokado313.mnm@gmail.com>
2025-04-04 22:32:35 +0900

    25/04/04 kotlinでぱいざ

--- src/PaizaChallenge.kt
+++ src/PaizaChallenge.kt
@@ -1,35 +1,61 @@
 fun main(args: Array<String>) {
-    val (h, w) = readln().split(" ").map { it.toInt() }
-    val picture = Array(h) { CharArray(w) }
-
-    for (i in 0 until h) {
-        picture[i] = readln().toCharArray()
+    val (n, k) = readln().split(" ").map { it.toInt() } // n: ビンゴカードの縦横　k: 抽選回数
+    val card: MutableList<MutableList<Int>> = MutableList(n) { mutableListOf() } // ビンゴカード
+    for (i in 0 until n) {
+        card[i] = readln().split(" ").map { it.toInt() }.toMutableList()
+//        println(card[i].joinToString(" "))
     }
 
-    var result = 0
-    // １行目と最終行の'.'はドーナッツにならない。
-    // 2行目からh-1行目までを走査する。
-    for (i in 1 until h - 1) {
-        // 同じく、１列目と最終列の'.'はドーナッツにならない。
-        // 2列目からw-1列目までを走査する。
-        for(j in 1 until w - 1) {
-            if (picture[i][j] == '.') {
-                // '.'の周囲８マスが全て'#'であればドーナッツと判定する。
-                if (picture[i - 1][j - 1] == '#' &&
-                    picture[i - 1][j] == '#' &&
-                    picture[i - 1][j + 1] == '#' &&
-                    picture[i][j - 1] == '#' &&
-                    picture[i][j + 1] == '#' &&
-                    picture[i + 1][j - 1] == '#' &&
-                    picture[i + 1][j] == '#' &&
-                    picture[i + 1][j + 1] == '#')
-                {
-                    result++
-                }
+    val drawNums = readln().split(" ").map { it.toInt() }
+
+    var bingoCount = 0
+
+    val colScanList: MutableList<Int> = MutableList(n) { 0 }
+
+    for (i in 0 until n) {
+        var rowSum = 0
+        for (j in 0 until n) {
+            // 抽選の数字と一致したら穴をあける = 0にする
+            if (drawNums.contains(card[i][j])) {
+                card[i][j] = 0
             }
+            rowSum += card[i][j]
+
+            // 列の合計に足し込む
+            colScanList.set(j, colScanList[j] + card[i][j])
+        }
+
+        // 行のビンゴ判定
+        if (rowSum == 0) {
+            bingoCount++
         }
     }
-    println(result)
+
+    // 列のビンゴ判定
+    for (col in colScanList) {
+        if (col == 0) {
+            bingoCount++
+        }
+    }
+
+    // 斜めのビンゴ判定
+    var leftDiagonalSum = 0
+    var rightDiagonalSum = 0
+    var m = 0
+    while (m < n) {
+        leftDiagonalSum += card[m][m]
+        rightDiagonalSum += card[m][n - m - 1]
+        m++
+    }
+    if (leftDiagonalSum == 0) {
+        bingoCount++
+    }
+    if (rightDiagonalSum== 0) {
+        bingoCount++
+    }
+
+    println(bingoCount)
+
 }
 
 
